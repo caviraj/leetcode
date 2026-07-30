35129e53f17087d688f64d8df6e3f1b45545b228
caviraj <ncaviraj@gmail.com>
2026-07-30 11:02:57 +0530

    Add 75_sort_colors.java - LeetCode Problem 75

---
+++ 75_sort_colors.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        for(int i =0;i<nums.length-1;i++){
+            for(int j =0; j <nums.length-1-i;j++){
+                if(nums[j]>nums[j+1]){
+                int s = nums[j];
+                nums[j]=nums[j+1];
+                nums[j+1]=s;}
+            }
+        }
+    }
+}
