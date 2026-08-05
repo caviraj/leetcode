d14b74025ce2044616b3cbd2be2c7fb812b0ef78
caviraj <ncaviraj@gmail.com>
2026-08-05 18:47:16 +0530

    Add 1306_minimum_absolute_difference.java - LeetCode Problem 1306

---
+++ 1306_minimum_absolute_difference.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        
+        List<List<Integer>> result = new ArrayList<>();
+        int minDiff = Integer.MAX_VALUE;
+        
+        for (int i = 1; i < arr.length; i++) {
+            int diff = arr[i] - arr[i - 1];
+            
+            if (diff < minDiff) {
+                minDiff = diff;
+                result.clear();
+                result.add(Arrays.asList(arr[i - 1], arr[i]));
+            } else if (diff == minDiff) {
+                result.add(Arrays.asList(arr[i - 1], arr[i]));
+            }
+        }
+        
+        return result;
+    }
+}
